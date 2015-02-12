c44b1346a35221e7d6033661b3fbb8e98e9f1da4
Mark Bussey <mark@curationexperts.com>
2015-02-11 22:18:15 -0800

    Upgrade to fits 0.8.4

--- install_fits.sh
+++ install_fits.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/install && cd ~/install
-wget -c http://fits.googlecode.com/files/fits-0.6.2.zip
-unzip fits-0.6.2.zip  
-chmod a+x fits-0.6.2/fits.sh  
-cp -r fits-0.6.2/* /usr/local/bin/
+wget -c http://projects.iq.harvard.edu/files/fits/files/fits-0.8.4_0.zip
+unzip fits-0.8.4_0.zip 
+chmod a+x fits-0.8.4/fits.sh  
+cp -r fits-0.8.4/* /usr/local/bin/
 ln -sf /usr/local/bin/fits.sh /usr/local/bin/fits
