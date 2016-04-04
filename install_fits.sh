44a3a4aa10f643289a4573d3e980e212df8a47b3
Mark Bussey <mark@curationexperts.com>
2016-04-04 17:20:44 -0500

    Remove bash setup scripts replaced by ansible roles

--- install_fits.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/install && cd ~/install
-wget -c http://projects.iq.harvard.edu/files/fits/files/fits-0.8.4_0.zip
-unzip fits-0.8.4_0.zip 
-chmod a+x fits-0.8.4/fits.sh  
-cp -r fits-0.8.4/* /usr/local/bin/
-ln -sf /usr/local/bin/fits.sh /usr/local/bin/fits
